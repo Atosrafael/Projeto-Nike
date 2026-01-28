94a63f18905008ce38311617280873aff86d84f5
Atosrafae <atossousa@outlook.com>
2026-01-28 18:12:56 -0300

    Primeiro Commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+
+
+function mudarVisual(cor, imagem){
+
+    body.style.background = cor
+    tenis.src = imagem
+}
